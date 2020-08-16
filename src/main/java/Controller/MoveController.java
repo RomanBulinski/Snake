f3cec5edbffc03f4b152549c4777553e5b0aca67
roman <aboandrus@gmail.com>
2020-08-16 18:52:01 +0200

    Code refactor

--- src/main/java/Controller/MoveController.java
+++ src/main/java/Controller/MoveController.java
@@ -5,7 +5,6 @@ import Model.*;
 
 import java.util.Arrays;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class MoveController {
 
@@ -16,119 +15,79 @@ public class MoveController {
     }
 
     public Snake move(Snake snake, MoveEnum moveEnum, Board board) {
+        int size = snake.getSnakeMap().size();
         Cell head = snake.getSnakeMap().get(1);
         int row = head.getRow();
         int column = head.getColumn();
         if (moveEnum == MoveEnum.RIGHT) {
             if (board.getCoreBoard()[row][column + 1] instanceof CellFood) {
-                snake.addPartSnakeBody(2, new CellSnakeBody(2, row, column));
+                snake.addPartSnakeBody(size+1, new CellSnakeBody(size+1, row, column));
             }
             if (snake.getSnakeMap().size() == 1) {
                 head.setColumn(column + 1);
             } else {
                 int tempRow = head.getRow();
                 int tempColumn = head.getColumn();
-                int tempRowBis;
-                int tempColumnBis;
                 head.setColumn(column + 1);
-                Set<Integer> keys = snake.getSnakeMap().keySet();
-                Object[] keysA = keys.toArray();
-                Arrays.sort(keysA);
-                for (Object key : keysA) {
-                    if ((Integer) key != 1) {
-                        Cell cell = snake.getSnakeMap().get(key);
-                        tempRowBis = cell.getRow();
-                        tempColumnBis = cell.getColumn();
-                        cell.setRow(tempRow);
-                        cell.setColumn(tempColumn);
-                        tempRow = tempRowBis;
-                        tempColumn = tempColumnBis;
-                    }
-                }
+                replaceCellQueue(snake, tempRow, tempColumn);
             }
         } else if (moveEnum == MoveEnum.LEFT) {
             if (board.getCoreBoard()[row][column - 1] instanceof CellFood) {
-                snake.addPartSnakeBody(2, new CellSnakeBody(2, row, column));
+                snake.addPartSnakeBody(size+1, new CellSnakeBody(size+1, row, column));
             }
             if (snake.getSnakeMap().size() == 1) {
                 head.setColumn(column - 1);
             } else {
                 int tempRow = head.getRow();
                 int tempColumn = head.getColumn();
-                int tempRowBis;
-                int tempColumnBis;
                 head.setColumn(column - 1);
-                Set<Integer> keys = snake.getSnakeMap().keySet();
-                Object[] keysA = keys.toArray();
-                Arrays.sort(keysA);
-                for (Object key : keysA) {
-                    if ((Integer) key != 1) {
-                        Cell cell = snake.getSnakeMap().get(key);
-                        tempRowBis = cell.getRow();
-                        tempColumnBis = cell.getColumn();
-                        cell.setRow(tempRow);
-                        cell.setColumn(tempColumn);
-                        tempRow = tempRowBis;
-                        tempColumn = tempColumnBis;
-                    }
-                }
+                replaceCellQueue(snake, tempRow, tempColumn);
             }
         } else if (moveEnum == MoveEnum.UP) {
             if (board.getCoreBoard()[row - 1][column] instanceof CellFood) {
-                snake.addPartSnakeBody(2, new CellSnakeBody(2, row, column));
+                snake.addPartSnakeBody(size+1, new CellSnakeBody(size+1, row, column));
             }
             if (snake.getSnakeMap().size() == 1) {
                 head.setRow(row - 1);
             } else {
                 int tempRow = head.getRow();
                 int tempColumn = head.getColumn();
-                int tempRowBis;
-                int tempColumnBis;
                 head.setRow(row - 1);
-                Set<Integer> keys = snake.getSnakeMap().keySet();
-                Object[] keysA = keys.toArray();
-                Arrays.sort(keysA);
-                for (Object key : keysA) {
-                    if ((Integer) key != 1) {
-                        Cell cell = snake.getSnakeMap().get(key);
-                        tempRowBis = cell.getRow();
-                        tempColumnBis = cell.getColumn();
-                        cell.setRow(tempRow);
-                        cell.setColumn(tempColumn);
-                        tempRow = tempRowBis;
-                        tempColumn = tempColumnBis;
-                    }
-                }
+                replaceCellQueue(snake, tempRow, tempColumn);
             }
         } else if (moveEnum == MoveEnum.DOWN) {
             if (board.getCoreBoard()[row + 1][column] instanceof CellFood) {
-                snake.addPartSnakeBody(2, new CellSnakeBody(2, row, column));
+                snake.addPartSnakeBody(size+1, new CellSnakeBody(size+1, row, column));
             }
             if (snake.getSnakeMap().size() == 1) {
                 head.setRow(row + 1);
             } else {
                 int tempRow = head.getRow();
                 int tempColumn = head.getColumn();
-                int tempRowBis;
-                int tempColumnBis;
                 head.setRow(row +1 );
-                Set<Integer> keys = snake.getSnakeMap().keySet();
-                Object[] keysA = keys.toArray();
-                Arrays.sort(keysA);
-                for (Object key : keysA) {
-                    if ((Integer) key != 1) {
-                        Cell cell = snake.getSnakeMap().get(key);
-                        tempRowBis = cell.getRow();
-                        tempColumnBis = cell.getColumn();
-                        cell.setRow(tempRow);
-                        cell.setColumn(tempColumn);
-                        tempRow = tempRowBis;
-                        tempColumn = tempColumnBis;
-                    }
-                }
+                replaceCellQueue(snake, tempRow, tempColumn);
             }
         }
         return snake;
+    }
+
+    private void replaceCellQueue(Snake snake, int tempRow, int tempColumn) {
+        int tempRowBis;
+        int tempColumnBis;
+        Set<Integer> keys = snake.getSnakeMap().keySet();
+        Object[] keysA = keys.toArray();
+        Arrays.sort(keysA);
+        for (Object key : keysA) {
+            if ((Integer) key != 1) {
+                Cell cell = snake.getSnakeMap().get(key);
+                tempRowBis = cell.getRow();
+                tempColumnBis = cell.getColumn();
+                cell.setRow(tempRow);
+                cell.setColumn(tempColumn);
+                tempRow = tempRowBis;
+                tempColumn = tempColumnBis;
+            }
+        }
     }
 
     public Snake getSnake() {
